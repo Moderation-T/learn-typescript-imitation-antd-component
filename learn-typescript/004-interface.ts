1c5f62f643d4e834f1a7cd4428e6aae352b3505b
yizhiyu <yizhiyu_web@163.com>
2020-12-09 10:01:08 +0800

    :pencil: interface 抽象验证 class 的方法

--- learn-typescript/004-interface.ts
+++ learn-typescript/004-interface.ts
@@ -9,4 +9,38 @@ interface Person {
 
 let me: Person = { id: 1, name: 'tang', age: 12 }
 
+/****************************************************/
+
 // 对类进行定义和抽象
+interface Radio {
+  // void 什么都不返回
+  switchRadio(): void
+}
+
+interface Battery {
+  checkBatteryStatus(): void
+}
+
+// interface 也可以继承
+interface RadioSon extends Radio {
+  extendsRadio(): void
+}
+
+// Extends 用于(单)继承一个类（class），而 implements 用于实现一个接口(interface)。 
+class Car implements Radio {
+  switchRadio() { }
+}
+
+// 实现两个接口用逗号隔开
+class Cellphone implements Battery, Radio {
+  switchRadio() {
+
+  };
+  checkBatteryStatus() { }
+
+}
+
+class ExtendsRadio implements RadioSon {
+  extendsRadio() { }
+  switchRadio() { }
+}
