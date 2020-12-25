90c51f50fb60b649d0fbb8228a86e464f60480d7
yizhiyu <yizhiyu_web@163.com>
2020-12-25 14:29:27 +0800

    :white_check_mark: test menu

--- initation-antd/src/container/MenuTest.tsx
+++ initation-antd/src/container/MenuTest.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { Fragment } from 'react';
 import Menu from '../components/Menu/menu'
 import MenuItem from '../components/Menu/menuItem';
 
 function MenuTest() {
   return (
-    <Menu>
-      <MenuItem value={'1'} >菜单1</MenuItem>
-      <MenuItem value={'2'} >菜单2</MenuItem>
-      <MenuItem value={'3'} >菜单3</MenuItem>
-      <MenuItem value={'4'} >菜单4</MenuItem>
-    </Menu>
+    <Fragment>
+      horizontal:
+      <Menu>
+        <MenuItem value={'1'} >菜单1</MenuItem>
+        <MenuItem value={'2'} >菜单2</MenuItem>
+        <MenuItem value={'3'} >菜单3</MenuItem>
+        <MenuItem value={'4'} >菜单4</MenuItem>
+      </Menu>
+      vertical:
+      <Menu mode='vertical'>
+        <MenuItem value={'1'} >菜单1</MenuItem>
+        <MenuItem value={'2'} >菜单2</MenuItem>
+        <MenuItem value={'3'} >菜单3</MenuItem>
+        <MenuItem value={'4'} >菜单4</MenuItem>
+      </Menu>
+    </Fragment>
   )
 }
-
 export default MenuTest;
